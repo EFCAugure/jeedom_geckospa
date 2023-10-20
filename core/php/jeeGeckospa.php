7a0ca42e25f03562189e52dc55891cbdcc19abc6
chris94440 <62375301+chris94440@users.noreply.github.com>
2023-10-20 15:20:37 +0200

    Add files via upload

---
+++ core/php/jeeGeckospa.php
@@ -0,0 +1,32 @@
+<?php
+
+try {
+    require_once dirname(__FILE__) . "/../../../../core/php/core.inc.php";
+
+    if (!jeedom::apiAccess(init('apikey'), 'geckospa')) { //remplacez template par l'id de votre plugin
+        echo __('Vous n\'etes pas autorisé à effectuer cette action', __FILE__);
+        die();
+    }
+    if (init('test') != '') {
+        echo 'OK';
+        die();
+    }
+    $result = json_decode(file_get_contents("php://input"), true);
+    if (!is_array($result)) {
+        die();
+    }
+
+    log::add('geckospa', 'debug', '*-----------------------------------------------------------------------------*');
+    if (isset($result['eventItem'])) {        
+        log::add('geckospa', 'debug', 'Message receive for evenItem -> ' . json_encode($result['eventItem']));
+        geckospa::updateItems($result['eventItem']);
+    } elseif (isset($result['devicesList'])) {
+        log::add('geckospa', 'debug', 'Message receive for devicesList -> ' . json_encode($result['devicesList']));
+        geckospa::create_or_update_devices($result['devicesList']);
+   //}else {
+    //    log::add('geckospa', 'error', 'unknown message received from daemon'); //remplacez template par l'id de votre plugin
+    }
+    log::add('geckospa', 'debug', '*-----------------------------------------------------------------------------*');
+} catch (Exception $e) {
+    log::add('geckospa', 'error', displayException($e)); //remplacez template par l'id de votre plugin
+}
